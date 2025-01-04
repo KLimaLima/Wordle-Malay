bfcf385541520499afb90dbe23313ef5cb162e2e
fauz4n01 <b022210165@student.utem.edu.my>
2025-01-04 19:36:40 +0800

    fixed attempt & leaderboard

--- src/game/leaderboard.js
+++ src/game/leaderboard.js
@@ -12,49 +12,49 @@ leaderboardRouter.get('/leaderboard', verify_jwt, jwt_search, input_word, async(
     let total_tries_1 = await game_data.countDocuments(
         {
             successful: true,
-            attempts: 1
+            attempt: 1
         }
     )
 
     let total_tries_2 = await game_data.countDocuments(
         {
             successful: true,
-            attempts: 2
+            attempt: 2
         }
     )
 
     let total_tries_3 = await game_data.countDocuments(
         {
             successful: true,
-            attempts: 3
+            attempt: 3
         }
     )
 
     let total_tries_4 = await game_data.countDocuments(
         {
             successful: true,
-            attempts: 4
+            attempt: 4
         }
     )
 
     let total_tries_5 = await game_data.countDocuments(
         {
             successful: true,
-            attempts: 5
+            attempt: 5
         }
     )
 
     const leaderboard_json = {
         description: 'Number of attempts that players have taken to get the answer',
-        one_attemp: total_tries_1,
-        two_attemp: total_tries_2,
-        three_attemp: total_tries_3,
-        four_attemp: total_tries_4,
+        one_attempt: total_tries_1,
+        two_attempt: total_tries_2,
+        three_attempt: total_tries_3,
+        four_attempt: total_tries_4,
         five_attempt: total_tries_5
     }
 
     const leaderboard_to_send = JSON.stringify(leaderboard_json)
-    res.send(leaderboard_to_send)
+    res.send(leaderboard_json)
 })
 
 module.exports = leaderboardRouter
