4ea012eb0187be912ded61db0634772ba161614c
fauz4n01 <b022210165@student.utem.edu.my>
2025-01-06 22:15:16 +0800

    add password policy

--- src/utils/validate-password.js
+++ src/utils/validate-password.js
@@ -1,6 +1,59 @@
 const bcrypt = require('bcrypt');
 salt_rounds = 10;
 
+const PASSWORD_POLICY = {
+    MIN_LENGTH: 8,
+    MAX_LENGTH: 128,
+    REQUIRE_NUMBER: true,
+    REQUIRE_SPECIAL: true,
+    REQUIRE_UPPER_LOWER: true
+}
+
+function validatePasswordPolicy(password) {
+
+    const errors = [];
+
+    if (password.length < PASSWORD_POLICY.MIN_LENGTH) {
+        errors.push(`Password must be at least ${PASSWORD_POLICY.MIN_LENGTH} characters\n`);
+    }
+
+    if (password.length > PASSWORD_POLICY.MAX_LENGTH) {
+        errors.push(`Password must be at most ${PASSWORD_POLICY.MAX_LENGTH} characters\n`);
+    }
+
+    if (PASSWORD_POLICY.REQUIRE_NUMBER && !password.match(/[0-9]/)) {
+        errors.push('Password must contain at least one number\n');
+    }
+
+    if (PASSWORD_POLICY.REQUIRE_SPECIAL && !password.match(/[^a-zA-Z0-9]/)) {
+        errors.push('Password must contain at least one special character\n');
+    }
+
+    if (PASSWORD_POLICY.REQUIRE_UPPER_LOWER && !(password.match(/[a-z]/) && password.match(/[A-Z]/))) {
+        errors.push('Password must contain at least one uppercase and one lowercase letter\n');
+    }
+
+    return errors;
+}
+
+function enforcePasswordPolicy(req, res, next) {
+    const { password } = req.body;
+
+    if (!password) {
+        res.status(400).send('Password is required.');
+        return;
+    }
+
+    const policyErrors = validatePasswordPolicy(password);
+
+    if (policyErrors.length > 0) {
+        res.status(400).send(`Password does not meet  requirements: ${policyErrors.join(' ')}`);
+        return;
+    }
+
+    next();
+}
+
 function validate_password(req, res, next) {
 
     let { password } = req.body
@@ -26,4 +79,4 @@ function validate_password(req, res, next) {
     next()
 }
 
-module.exports = { validate_password }
+module.exports = { validate_password, enforcePasswordPolicy }
