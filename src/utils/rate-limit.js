b6050e6cf66a85d1bb28203fc8ebea4f94bf7b18
fauz4n01 <b022210165@student.utem.edu.my>
2025-01-07 00:04:33 +0800

    added rate limiting

---
+++ src/utils/rate-limit.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 5,
+    message: 'Too many requests from this IP, please try again after a minute',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+module.exports = { limiter };
