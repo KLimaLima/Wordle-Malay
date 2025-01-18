476c0199620d49c1f26965615d43b66ca4627d53
fauz4n01 <b022210165@student.utem.edu.my>
2025-01-18 17:08:38 +0800

    remove bracket to import func

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -1,63 +1,14 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
-app.use(express.json());
 
-const { ip_addr } = require('../db/client.js');
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 5,
+    message: 'Too many requests from this IP, please try again after a minute',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
-const MAX_REQUESTS = 20;
-const WINDOW_MS = 1 * 60 * 1000;
-
-const limiter = async (req, res, next) => {
-    const userIP = req.ip;
-    const currentTime = Date.now();
-
-    try {
-        // Fetch the record for the IP
-        const record = await ip_addr.findOne({ ip: userIP });
-
-        if (!record) {
-            // New IP: Insert a new record
-            await ip_addr.insertOne({
-                ip: userIP,
-                requestCount: 1,
-                firstRequestTimestamp: currentTime,
-            });
-            next();
-        } else {
-            // Existing IP: Check request count and time window
-            const timeElapsed = currentTime - record.firstRequestTimestamp;
-
-            if (timeElapsed < WINDOW_MS) {
-                // Within time window
-                if (record.requestCount >= MAX_REQUESTS) {
-                    res.status(429).send('Too many requests. Please try again later.');
-                } else {
-                    // Increment request count
-                    await ip_addr.updateOne(
-                        { ip: userIP },
-                        { $inc: { requestCount: 1 } }
-                    );
-                    next();
-                }
-            } else {
-                // Outside time window: Reset request count and timestamp
-                await ip_addr.updateOne(
-                    { ip: userIP },
-                    {
-                        $set: {
-                            requestCount: 1,
-                            firstRequestTimestamp: currentTime,
-                        },
-                    }
-                );
-                next();
-            }
-        }
-    } catch (error) {
-        console.error('Rate limiter error:', error);
-        res.status(500).send('Internal server error.');
-    }
-};
-
-module.exports = { limiter };
+module.exports = limiter;
