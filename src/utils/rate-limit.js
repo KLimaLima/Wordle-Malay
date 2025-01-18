435ff5862066b51eba1b9e25d2841082884ae8d7
fauz4n01 <b022210165@student.utem.edu.my>
2025-01-18 15:38:08 +0800

    fixed rate limit based on ip

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -3,12 +3,13 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 30,
+    max: 20,
     message: 'Too many requests from this IP, please try again after a minute',
-    standardHeaders: true,
-    legacyHeaders: false,
+    keyGenerator: (req, res) => req.ip,
 });
 
 module.exports = { limiter };
