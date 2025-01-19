ecdd675d7f8eafb02b7d77660684743199ac14be
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-19 16:29:42 +0800

    change rate limit

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -6,8 +6,8 @@ const rateLimit = require('express-rate-limit');
 // app.use(requestIp.mw());
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
