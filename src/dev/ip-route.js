921fd89a7e9a882449353d2779b3ad7d7fbf41b2
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-20 10:21:25 +0800

    changed a few requirements

--- src/dev/ip-route.js
+++ src/dev/ip-route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const ipRouter = express.Router();
 
+const { verify_jwt } = require('../utils/verify-jwt.js');
+
 ipRouter.route('/dev/ip')
-    .get( async (req, res) => {
+    .get(verify_jwt, async (req, res) => {
         res.status(200).send(req.ip);
     });
 
