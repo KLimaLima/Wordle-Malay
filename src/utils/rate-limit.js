f4c84403a0e109be40a7f6fa2b29d324478de141
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2025-01-21 14:13:41 +0800

    Update rate-limit.js

--- src/utils/rate-limit.js
+++ src/utils/rate-limit.js
@@ -1,11 +1,20 @@
 const rateLimit = require('express-rate-limit');
 
+function give_ip(req, res) {
+
+	let string_ip = req.ip
+
+	string_ip = string_ip.split(":", 1)
+
+	return string_ip
+}
+
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minutes
 	limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-	keyGenerator: (req, res) => req.ip.split(":", 1),
+	keyGenerator: (req, res) => give_ip,
 })
 
 module.exports = limiter;
